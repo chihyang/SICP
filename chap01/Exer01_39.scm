1218c84488345c8c3aaa26ac6375c07c1a3a9fe8
chihyang <chihyanghsin@gmail.com>
2016-01-26 22:35:50 +0800

    Modify exercise 1.38 and 1.39.

--- chap01/Exer01_39.scm
+++ chap01/Exer01_39.scm
@@ -1,9 +1,8 @@
 (load "Sect01_17_square.scm")
 (load "Exer01_37_cont_frac.scm")
 (define (tan-cf x k)
-  ( /
-    (cont-frac (lambda (i) (- (square x)))
-             (lambda (i) (- (* 2 i) 1))
-             k)
-    (- x)))
-  
+    (cont-frac (lambda (i) (if (= i 1)
+                               x
+                               (- (square x))))
+               (lambda (i) (- (* 2 i) 1))
+               k))
