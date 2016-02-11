415237e681829d26b05d454455f14862a7d4e923
chihyang <chihyanghsin@gmail.com>
2016-02-11 23:20:18 +0800

    Exercise 2.12.

---
+++ chap02/Exer02_11_make_center_width.scm
@@ -0,0 +1,7 @@
+(load "Exer02_07.scm")
+(define (make-center-width c w)
+  (make-interval (-c w) (c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
---
+++ chap02/Exer02_12_make_center_percent.scm
@@ -0,0 +1,8 @@
+(load "Exer02_07.scm")
+(define (make-center-percent c p)
+  (make-interval (* c (- 1 p)) (* c (+ 1 p))))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (percent i)
+  (/ (- (upper-bound i) (lower-bound i))
+     (* 2 (center i))))
