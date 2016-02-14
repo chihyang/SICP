240e3c0111b3e9ee93f3d6c9a7d5fc1cde4ad479
chihyang <chihyanghsin@gmail.com>
2016-02-14 20:51:55 +0800

    Fix error in README.md, Exer02_07.scm and Sect02_14_interval_arithmetic.scm.

--- chap02/Sect02_14_interval_arithmetic.scm
+++ chap02/Sect02_14_interval_arithmetic.scm
@@ -3,12 +3,12 @@
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound y) (upper-bound y))))
 (define (mul-interval x y)
-  (let ((p1 (* lower-bound x) (lower-bound y)))
-    (p2 (* (lower-bound x) (upper-bound y)))
-    (p3 (* (upper-bound x) (lower-bound y)))
-    (p4 (* (upper-bound x) (upper-bound y))))
-  (make-interval (min p1 p2 p3 p4)
-                 (max p1 p2 p3 p4)))
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
 (define (div-interval x y)
   (mul-interval
    x
