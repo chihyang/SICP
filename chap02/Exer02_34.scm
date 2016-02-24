12c6a0f10d301aa973f03256bc03e06eddc5b738
chihyang <chihyanghsin@gmail.com>
2016-02-24 16:13:05 +0800

    Exercise 2.34 and 2.35.

---
+++ chap02/Exer02_34.scm
@@ -0,0 +1,5 @@
+(load "Sect02_23.scm")
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ (* x higher-terms) this-coeff))
+              0
+              coefficient-sequence))
