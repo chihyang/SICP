da2ed2a31f3c7858e5782517370cef93c2865f17
chihyang <chihyanghsin@gmail.com>
2016-02-24 20:40:05 +0800

    Exercise 2.36.

---
+++ chap02/Exer02_36.scm
@@ -0,0 +1,6 @@
+(load "Sect02_23.scm")
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (accumulate (lambda (x y) (cons (car x) y)) '() seqs))
+            (accumulate-n op init (accumulate (lambda (x y) (cons (cdr x) y)) '() seqs)))))
