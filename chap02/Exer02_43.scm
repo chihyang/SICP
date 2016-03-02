708f7396582ac6cc475329ab81a9889aa063f4ee
chihyang <chihyanghsin@gmail.com>
2016-03-02 21:33:44 +0800

    Add exercise 2.43.

--- chap02/Exer02_43.scm
+++ chap02/Exer02_43.scm
@@ -6,12 +6,12 @@
         (filter
          (lambda (positions) (safe? k positions))
          (flatmap
-          (lambda (rest-of-queens)
-            (map (lambda (new-row)
+          (lambda (new-row)
+            (map (lambda (rest-of-queens)
                    (adjoin-position
                     new-row k rest-of-queens))
-                 (enumerate-interval 1 board-size)))
-          (queen-cols (- k 1))))))
+                 (queen-cols (- k 1))))
+          (enumerate-interval 1 board-size)))))
   (queen-cols board-size))
 (define (adjoin-position row col board)
   (if (null? board)
