55dfaabf628aaa43069e294854a32e3326049e99
chihyang <chihyanghsin@gmail.com>
2016-03-03 20:39:43 +0800

    Exercise 2.46 and 2.47.

---
+++ chap02/Sect02_24_frame_coord_map.scm
@@ -0,0 +1,8 @@
+(load "Exer02_46_cons.scm")
+(load "Exer02_47_cons.scm")
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+               (scale-vect (ycor-vect v) (edge2-frame frame))))))
