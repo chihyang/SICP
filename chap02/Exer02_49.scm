c11d3a6a17ca83bfc5f23237f5d2828194b3bd17
chihyang <chihyanghsin@gmail.com>
2016-03-04 22:04:51 +0800

    Complete wave in exercise 2.49.
    Use data from huangz1990(github).

--- chap02/Exer02_49.scm
+++ chap02/Exer02_49.scm
@@ -1,4 +1,4 @@
-(load "Sect02_24_segments_painter.scm")
+ (load "Sect02_24_segments_painter.scm")
 (define unit-vect1 (make-vect 0 0))
 (define unit-vect2 (make-vect 0 1))
 (define unit-vect3 (make-vect 1 1))
@@ -16,5 +16,23 @@
                            (make-segment (make-vect 0.5 1) (make-vect 1 0.5))
                            (make-segment (make-vect 1 0.5) (make-vect 0.5 0))
                            (make-segment (make-vect 0.5 0) (make-vect 0 0.5)))))
-(define (wave segment-list)
-  (segments->painter segment-list))
+(define wave
+  (segments->painter
+   (list (make-segment (make-vect 0.4 1.0) (make-vect 0.35 0.85)); 头部左上
+         (make-segment (make-vect 0.35 0.85) (make-vect 0.4 0.64))   ; 头部左下
+         (make-segment (make-vect 0.4 0.65) (make-vect 0.25 0.65))    ; 左肩
+         (make-segment (make-vect 0.25 0.65) (make-vect 0.15 0.6))  ; 左手臂上部
+         (make-segment (make-vect 0.15 0.6) (make-vect 0.0 0.85))  ; 左手上部
+         (make-segment (make-vect 0.0 0.65) (make-vect 0.15 0.35))    ; 左手下部
+         (make-segment (make-vect 0.15 0.35) (make-vect 0.25 0.6))   ; 左手臂下部
+         (make-segment (make-vect 0.25 0.6) (make-vect 0.35 0.5))     ; 左边身体
+         (make-segment (make-vect 0.35 0.5) (make-vect 0.25 0.0))    ; 左腿外侧
+         (make-segment (make-vect 0.6 1.0) (make-vect 0.65 0.85))     ; 头部右上
+         (make-segment (make-vect 0.65 0.85) (make-vect 0.6 0.65))  ; 头部右下
+         (make-segment (make-vect 0.6 0.65) (make-vect 0.75 0.65))    ; 右肩
+         (make-segment (make-vect 0.75 0.65) (make-vect 1.0 0.3))   ; 右手上部
+         (make-segment (make-vect 1.0 0.15) (make-vect 0.6 0.5))    ; 右手下部
+         (make-segment (make-vect 0.6 0.5) (make-vect 0.75 0.0))    ; 右腿外侧
+         (make-segment (make-vect 0.4 0.0) (make-vect 0.5 0.3))    ; 左腿内侧
+         (make-segment (make-vect 0.6 0.0) (make-vect 0.5 0.3))     ; 右腿内侧
+         )))
