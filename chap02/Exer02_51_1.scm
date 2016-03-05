0bca1e61ecedd0b51337eceb1a89f388b7aefcef
chihyang <chihyanghsin@gmail.com>
2016-03-05 19:34:21 +0800

    Fix error in exercise 2.51.
    - For the first way, the frame must be constructed clockwise;
    - For the second way, the figure must be rotated by 90 degree after calling beside. Draw a picture and it could be verified.

--- chap02/Exer02_51_1.scm
+++ chap02/Exer02_51_1.scm
@@ -5,13 +5,14 @@
            (transform-painter
             painter1
             split-point
-            (make-vect 0.0 1.0)
-            (make-vect 1.0 0.5)))
+            (make-vect 1.0 0.5)
+            (make-vect 0.0 1.0)))
           (paint-down
            (transform-painter
+            painter2
             (make-vect 0.0 0.0)
-            split-point
-            (make-vect 1.0 0.0))))
+            (make-vect 1.0 0.0)
+            split-point)))
       (lambda (frame)
         (paint-up frame)
         (paint-down frame)))))
