492f9ad39685f6749f3fbe320ab30b06eed59485
chihyang <chihyanghsin@gmail.com>
2016-03-08 21:53:24 +0800

    Add differentiation of exp in section 2.3.2.

--- chap02/Sect02_32_deriv.scm
+++ chap02/Sect02_32_deriv.scm
@@ -1,4 +1,5 @@
 (load "Sect02_32_algebraic_expression.scm")
+(load "Exer02_56.scm")
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
@@ -10,5 +11,11 @@
                         (deriv (multiplicand exp) var))
           (make-product (deriv (multiplier exp) var)
                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+           (exponent exp)
+           (make-product
+            (make-exponentiation (base exp) (make-sum (exponent exp) -1))
+            (deriv (base exp) var))))
         (else
          (error "unknown expression type: DERIV" exp))))
