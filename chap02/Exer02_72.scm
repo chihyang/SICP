c58c0fbff4994c157750dd28d27d9607383d2263
chihyang <chihyanghsin@gmail.com>
2016-03-25 20:50:36 +0800

    Exercise 2.72.

---
+++ chap02/Exer02_72.scm
@@ -0,0 +1,7 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp))
+               (operands exp) var))))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
