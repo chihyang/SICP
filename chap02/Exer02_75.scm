4034eafc0c35b833d942e8fdcab1fa2c5d65cb29
chihyang <chihyanghsin@gmail.com>
2016-05-14 19:16:17 +0800

    Section 2.5.1.

---
+++ chap02/Exer02_75.scm
@@ -0,0 +1,8 @@
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'angle) a)
+          ((eq? op 'magnitude) r)
+          (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
